e8bead357d7b1c70e2c325377a481fefd6e3422a
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-07 14:59:59 +0500

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/signup', productController.signup); 
-router.get('/allUsers/', productController.allUsers);
+router.get('/allusers/', productController.allUsers);
 router.get('/getuser/:query', productController.getUser);
-router.put('/updateUser/:id', productController.updateUser);
-router.delete('/deleteUser/:id', productController.deleteUser);
+router.get('/pendingaccounts', productController.pendingAccount);
+router.get('/expiredaccounts', productController.expiredAccount);
+router.get('/activeaccounts', productController.activeAccount);
+router.put('/approveuser/:id', productController.approveUser);
+router.put('/updateuser/:id', productController.updateUser);
+router.delete('/deleteuser/:id', productController.deleteUser);
 
 module.exports = router;
