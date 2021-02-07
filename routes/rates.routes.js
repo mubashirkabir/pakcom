916b36ffcd8103711ed7f93a23773c4f30605514
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-07 10:16:03 +0500

    Update rates.routes.js

--- routes/rates.routes.js
+++ routes/rates.routes.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 router.post('/addRate', ratesController.addRates);
 router.get('/allRates/', ratesController.allRates);
+router.get('/categoryRates/:category', ratesController.allCategoryRates);
+router.get('/getoldrates/', ratesController.getOldRates);
 router.put('/updateRate/:id', ratesController.updateRates);
 router.put('/updateSequence/:id/:otherid', ratesController.updateSequence);
 router.delete('/deleteRate/:id', ratesController.deleteRates);
