9a725041614a8ca1820cc0c6a3b4c9790255b9a0
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-08 10:15:55 +0500

    Update blogs.routes.js

--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -3,7 +3,12 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/addblog', blogController.addBlog);
-router.get('/allblog/', blogController.allBlogs);
+router.get('/allblog/', blogController.allBlog); //will return eng & urdu 
+router.get('/allblog/:id', blogController.allBlogCategory);  // will return eng + urdu w.r.t category
+router.get('/allblogeng/', blogController.allBlogsEng); // will return only eng content
+router.get('/allblogurdu/', blogController.allBlogsUrdu);// will return only urdu content
+router.get('/blogcategorycng/:id', blogController.blogCategoryEng);// will return only eng content w.r.t category
+router.get('/blogcategoryurdu/:id', blogController.blogCategoryUrdu);// will return only urdu content w.r.t category
 router.put('/updateblog/:id', blogController.updateBlog);
 router.put('/makePublic/:id', blogController.makePublic);
 router.put('/makePrivate/:id', blogController.makePrivate);
