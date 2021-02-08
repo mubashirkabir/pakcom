4f900cb327abc93ee0ded80bc1f5590e00a00021
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-08 07:10:08 +0500

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,11 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/signup', productController.signup); 
+router.post('/signin', productController.signin); 
 router.get('/allusers/', productController.allUsers);
 router.get('/getuser/:query', productController.getUser);
 router.get('/pendingaccounts', productController.pendingAccount);
 router.get('/expiredaccounts', productController.expiredAccount);
 router.get('/activeaccounts', productController.activeAccount);
+router.put('/forgotpassword/:id', productController.forgotPassword);
 router.put('/approveuser/:id', productController.approveUser);
 router.put('/disableuser/:id', productController.disableUser);
 router.put('/updateuser/:id', productController.updateUser);
