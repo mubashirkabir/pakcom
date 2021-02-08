c641b4bc26637cd26618ef4a9f5e742cfa4ddd4f
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-08 10:42:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require('config');
 var mongoose = require('mongoose');
+var cors = require('cors');
 var productRouter = require('./routes/product.routes');
 var categoryRouter = require('./routes/category.routes');
 var locationRouter = require('./routes/location.routes');
@@ -18,6 +19,7 @@ create_db_connection();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +32,7 @@ app.use('/location', locationRouter);
 app.use('/rates', ratesRouter);
 app.use('/plan', planRouter);
 app.use('/user', userRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
