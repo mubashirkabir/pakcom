35de14044c63cf01281a85d1e80d89f2de29d081
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-08 11:01:45 +0500

    Update notification.routes.js

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 router.get('/allnotifications/', notificationController.allNotifications);
 router.get('/adminNotifications/', notificationController.adminNotifications);
+router.get('/userNotifications/', notificationController.userNotifications);
 router.get('/approvalNotifications/', notificationController.approvalNotifications);
 router.get('/blogsNotifications/', notificationController.blogsNotifications);
+router.get('/screenshotNotifications/', notificationController.screenshotNotifications);
 
 module.exports = router;
