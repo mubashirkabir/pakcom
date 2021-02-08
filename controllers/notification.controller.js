55bc78e7d1c6c7f4aa72fb0582ae43180986db1e
Mubashir Kabir <30483326+mubashirkabir@users.noreply.github.com>
2021-02-08 10:57:57 +0500

    Add files via upload

---
+++ controllers/notification.controller.js
@@ -0,0 +1,37 @@
+var notificationModel = require("../models/notification")
+
+var notificationController = {
+    allNotifications: (req, res) => {
+        notificationModel.find({}).exec(function (error, results) {
+            if (error) {
+                return next(error);
+            }
+            res.json(results);
+        });
+    },
+    adminNotifications: (req, res) => {
+        notificationModel.find({"notificationFor" : "admin"}).exec(function (error, results) {
+            if (error) {
+                return next(error);
+            }
+            res.json(results);
+        });
+    },
+    approvalNotifications: (req, res) => {
+        notificationModel.find({"notificationType" : "userApproval"}).exec(function (error, results) {
+            if (error) {
+                return next(error);
+            }
+            res.json(results);
+        });
+    },
+    blogsNotifications: (req, res) => {
+        notificationModel.find({ $or: [{ "notificationType": "blog Added" }, { "notificationType": "blog Updated" }] }).exec(function (error, results) {
+            if (error) {
+                return next(error);
+            }
+            res.json(results);
+        });
+    }
+}
+module.exports = notificationController
