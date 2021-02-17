edb28c916129a2e3b918f9b4eca765129ec01345
Mubashir Kabir <mubashirk97@hotmail.com>
2021-02-17 07:44:38 +0500

    product updated

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -6,6 +6,6 @@ router.post('/addproduct', productController.addProduct);
 router.get('/allProducts/', productController.allProducts);
 router.get('/allcategoryProducts/:categoryId', productController.allCategoryProducts);
 router.put('/updateProduct/:id', productController.updateProduct);
-router.delete('/deleteProduct/:id', productController.deleteProduct);
+router.delete('//:id', productController.deleteProduct);
 
 module.exports = router;
